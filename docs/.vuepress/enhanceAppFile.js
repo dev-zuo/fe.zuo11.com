fa52ae96d456c635491cb85ae59da11603e69f07
左小白 <guoqzuo@gmail.com>
2022-04-08 00:03:10 +0800

    add zuo-statistics 数据上报

--- docs/.vuepress/enhanceAppFile.js
+++ docs/.vuepress/enhanceAppFile.js
@@ -10,6 +10,16 @@ export default ({ router, Vue}) => {
           var s = document.getElementsByTagName("script")[0];
           s.parentNode.insertBefore(hm, s);
         })();
+        
+        <script>
+          var _hmt = _hmt || [];
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "http://zuo11.com:3000/zs.js?283281668cc3440449274d1f93c04de6";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+          })();
+        </script>
 
     //     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9527676606416641"
     //  crossorigin="anonymous"></script>
