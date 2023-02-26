1db8765173994eccfaa3be5a58b14981b2ef8573
zuoxiaobai <guoqzuo@gmail.com>
2023-02-26 20:37:01 +0800

    feat: update head first vue chapter 2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -534,6 +534,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['link', { rel: 'stylesheet', href: '/common.css' }], // 添加自定义样式
     ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     ['script', {},
